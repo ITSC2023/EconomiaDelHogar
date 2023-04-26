b3f26904568a6e9e0ed94953a64b37289a3acd17
AnaliaBauer <bauerany@gmail.com>
2023-04-25 23:30:54 -0300

    Creacion de Modelos

---
+++ EconomiaDelHogar/EDH.Comun/Modelos/Tarjeta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDH.Comun.Modelos
+{
+    internal class Tarjeta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string EnteEmisor { get; set; }
+        public DateTime Cierre { get; set; }
+        public DateTime? VencimientoResumen { get; set; }
+        public decimal Importe { get; set; }
+    }
+}

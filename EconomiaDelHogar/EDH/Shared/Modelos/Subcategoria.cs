5643d4e92cfd8f2e31d6863c79fafb7970cb7c4d
AnaliaBauer <bauerany@gmail.com>
2023-04-26 11:35:09 -0300

    Nuevos cambios en la estructura del proyecto

---
+++ EconomiaDelHogar/EDH/Shared/Modelos/Subcategoria.cs
@@ -0,0 +1,9 @@
+﻿namespace EDH.Comun.Modelos
+{
+    public class Subcategoria
+    {
+        public int Id { get; set; }
+        public string Nombre  { get; set; }
+        public string Color { get; set;}
+    }
+}
